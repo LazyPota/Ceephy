020a3fa6ae0cdb3e2f7b183a494b2d0d053d3bd6
LazyPota <raflipersada618@gmail.com>
2024-07-12 17:02:59 +0700

    okasd

--- register/regist.js
+++ register/regist.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/registration', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('https://ceephy.vercel.app/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('MongoDB connection error:', error));
 
