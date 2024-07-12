37e4efb2119381b4468f19071dd49631cdf06466
LazyPota <raflipersada618@gmail.com>
2024-07-13 05:45:15 +0700

    0123u2810u31

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, '/Public/home.html'));
   });
   
 app.use(express.static("Public"));
