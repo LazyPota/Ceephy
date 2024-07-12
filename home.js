509b8b798f6f70490f504bbaf26ddf4c95863a5f
LazyPota <raflipersada618@gmail.com>
2024-07-13 04:06:21 +0700

    23

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'home.html'));
+    res.sendFile(path.join(__dirname, 'D:\Ceephy Hackathon\home.html'));
   });
 
 app.listen(port, () => {
