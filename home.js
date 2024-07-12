f0ac79bdea645c6d8740575498f00869a2c1ae4f
LazyPota <raflipersada618@gmail.com>
2024-07-13 04:49:11 +0700

    html last commit

--- home.js
+++ home.js
@@ -4,10 +4,35 @@ require("dotenv").config();
 const port = process.env.PORT || 8000
 const path = require('path');
 
-// sendFile will go here
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public'));
-  });
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Profile page route
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'profile.html'));
+});
+
+// Comu page route
+app.get('/comu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'comu.html'));
+});
+
+// Portfolio page route
+app.get('/portfolio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'portfolio.html'));
+});
+
+// Jobs page route
+app.get('/jobs', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'jobs.html'));
+});
+
+// Setting page route
+app.get('/setting', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'setting.html'));
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: $(port)`);
