00d8c0893cc0bb4465676349f8333457ee50756d
LazyPota <raflipersada618@gmail.com>
2024-07-12 15:59:58 +0700

    clean

--- Home/home.js
+++ Home/home.js
@@ -1,4 +1,13 @@
-const APILINK = 
-const IMG_PATH =
-const SEARCHAPI =
+const express =  require("express");
+const cors = require("cors")
+const app = express();
+app.use(cors())
+require('dotenv').config();
+const port = process.env.PORT || 8000
 
+app.get("/", (req, res) => {
+    res.send("home");
+})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
