c050cb5628875676e2197cef15ba0f0409527434
LazyPota <raflipersada618@gmail.com>
2024-07-13 04:23:24 +0700

    242

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, '/home.html', '/home.css'));
   });
 
 app.listen(port, () => {
