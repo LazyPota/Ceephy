66cfb0f20909ecc54436f1348402c1fed88d3780
LazyPota <raflipersada618@gmail.com>
2024-07-13 04:09:58 +0700

    23232

--- home.js
+++ home.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000
+const path = '/home.html'
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile('home.html'.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, 'home'));
   });
 
 app.listen(port, () => {
