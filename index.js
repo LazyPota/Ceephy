e797dbb599a9e6b6affce0b1e8d708cb8980cfbd
LazyPota <raflipersada618@gmail.com>
2024-07-13 06:22:14 +0700

    jj

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-
-const PORT = 8080;
+const port = process.env.PORT || 8000
+require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"))
-app.use(require('./app/htmlRoutes/home.js')); 
+app.use(require('.htmlRoutes/home.js')); 
 
+app.listen (port, () => {
+    console.log(`Server is running on port: $(port)`)
+})
