dec06f4b259d214c5743b1eac90a2f079fd78fcb
LazyPota <raflipersada618@gmail.com>
2024-07-13 04:39:23 +0700

    232

--- home.js
+++ home.js
@@ -6,10 +6,8 @@ const path = require('path');
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, 'public'));
   });
-
-app.use(express.static("public"))
 
 app.listen(port, () => {
     console.log(`Server is running on port: $(port)`);
