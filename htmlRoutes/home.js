f7ab6342d3d4c58f164dad423cfd4c40ab8b28f8
LazyPota <raflipersada618@gmail.com>
2024-07-13 06:29:57 +0700

    ljdjksa

--- htmlRoutes/home.js
+++ htmlRoutes/home.js
@@ -3,11 +3,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.sendFile('home.html', { root: path.join(__dirname, '../public/home.html') });
+    res.sendFile('home.html', { root: path.join(__dirname, 'public\home.html') });
 });
 
 router.get('/commu', function(req, res) {
-    res.sendFile('commu.html', { root: path.join(__dirname, '../public/commu.html') });
+    res.sendFile('commu.html', { root: path.join(__dirname, 'public\commu.html') });
+});
+
+router.get('/setting', function(req, res) {
+  res.sendFile('setting.html', { root: path.join(__dirname, 'public\commu.html') });
+});
+
+router.get('/job', function(req, res) {
+  res.sendFile('profile.html', { root: path.join(__dirname, 'public\commu.html') });
+});
+
+router.get('/profile', function(req, res) {
+  res.sendFile('profile.html', { root: path.join(__dirname, 'public\commu.html') });
 });
 
 
