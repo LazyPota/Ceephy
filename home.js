91d56749c84f9b93681712cddaf8be31be1e2ae4
LazyPota <raflipersada618@gmail.com>
2024-07-12 12:38:24 +0700

    vercel

--- home.js
+++ home.js
@@ -1,18 +1,13 @@
 const express =  require("express");
-const cors = require("cors");
+const cors = require("cors")
 const app = express();
 app.use(cors())
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT || 8000
-const {readdirSync} = require("fs");
 
-readdirSync("./Routes").map((file)=>app.use("/",require("./Routes"+file)))
-
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("home");
 })
-
-app.get("/home", home);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
