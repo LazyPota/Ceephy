cb43236d7f659fce5c0d476e778310be60c2a26b
LazyPota <raflipersada618@gmail.com>
2024-07-13 05:41:30 +0700

    `192918`

--- home.js
+++ home.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 const port = process.env.PORT || 8000
 const path = require('path');
 
-app.use(express.static("Public"))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/home.html'));
+  });
+  
+app.use(express.static("Public"));
 
 app.listen(port, () => {
     console.log(`Server is running on port: $(port)`);
