33cb6ed5071fd5aed8f652114e4c27a950186d1f
LazyPota <raflipersada618@gmail.com>
2024-07-13 00:50:36 +0700

    gjv

--- Home/home.js
+++ Home/home.js
@@ -4,9 +4,8 @@ const cors = require("cors")
 app.use(cors())
 require("dotenv").config();
 const port = process.env.PORT || 8000
-const {readdirSync} = require("fs")
 
-readdirSync("./").map((file)=>app.use("/",require(file)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
