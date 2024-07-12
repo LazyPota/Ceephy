ed520d3d5ad2b40d141ee482f588b0f700396db6
LazyPota <raflipersada618@gmail.com>
2024-07-13 03:53:10 +0700

    2

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8000
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/home.html'));
+    res.sendFile(path.join(__dirname, 'home.html'));
   });
 
-  app.listen(PORT, () => console.log("Server ready on ($PORT)"));
+  app.listen(PORT, () => console.log("Server ready on port"+ PORT));
